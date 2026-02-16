ae85bbab74674b8b6058d4b1c58fa87292499822
Sanjan B M <acharyasanjan931@gmail.com>
2026-02-16 12:42:24 +0530

    Add service worker registration to enable offline support

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { registerServiceWorker } from "./utils/serviceWorker";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,3 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
+
+// Register service worker for offline support
+registerServiceWorker();
